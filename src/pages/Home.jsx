8b272737925dca51716521c68aae748169a37c10
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-09-16 20:10:36 +0000

    began adding test functionality for buttons with names on them

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 export const Home = () => {
   const { store, dispatch } = useGlobalReducer();
+  const [user, setUser] = useState("Sean");
 
   return (
     <div className="text-center mt-5">
@@ -13,6 +14,8 @@ export const Home = () => {
         <img src={rigoImageUrl} />
       </p>
       <Link to="/test">Go To Test Page</Link>
+      <button className="btn btn-primary" onClick={()=>setUser("Alex")} >{store.fname}</button>
+      <button className="btn btn-primary" onClick={()=>setUser("Ayala")} >{store.lname}</button>
     </div>
   );
 };
