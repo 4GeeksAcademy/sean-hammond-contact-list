10b8ead410f7f7bb6e73c106198914936855d307
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-09-16 20:00:59 +0000

    added useGlobalReducer code to Test.jsx

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const Test = () => {
+  const { store, dispatch } = useGlobalReducer();
   return <div>test</div>;
 };
