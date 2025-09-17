bda7d8dd3235c29eccc330dd8ecfae15eea8e10b
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-09-17 23:33:26 +0000

    trying the contact list project on my own

--- src/pages/CreateContact.jsx
+++ src/pages/CreateContact.jsx
@@ -22,17 +22,43 @@ export const CreateContact = () => {
 
   return (
     <div>
-      <h1>
-        {store.fname} {store.lname}
-      </h1>
-      <p>page where user enters contact info</p>
+      <h1>Add a new contact</h1>
+
+      {/* Name: */}
+      <label for="fullname">Full Name</label>
+      <br />
+      <input type="text" name="fullname" id="fullname" placeholder="First and Last Name" size="50" required />
+      <br />
+
+      {/* Email: */}
+      <label for="email">Email</label>
+      <br />
+      <input type="email" name="email" id="email" placeholder="Email Address" size="50" required />
+      <br />
+
+      {/* Phone: */}
+      <label for="phone">Phone</label>
+      <br />
+      <input type="tel" name="phone" id="phone" placeholder="Phone Number" size="50" required />
+      <br />
+
+      {/* Address: */}
+      <label for="fullname">Address</label>
+      <br />
+      <input type="text" name="fullname" id="fullname" placeholder="Mailing Address" size="50" required />
+      <br />
+
       <button
         onClick={() => {
           createContacts();
         }}
+        href="Home.jsx"
       >
-        Create Contact
+        Save contact
       </button>
+      <br />
+
+      <a href="Home.jsx">Back to contact list</a>
     </div>
   );
 };
