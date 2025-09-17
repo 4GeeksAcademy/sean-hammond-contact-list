62205b100b0a8f7a32f157efaeb28ade32a825a1
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-09-17 18:34:29 +0000

    2:32 in "contact list pt1 day1" video

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,26 @@ export const Home = () => {
   const { store, dispatch } = useGlobalReducer();
   const [user, setUser] = useState("Sean");
 
+  const createAgenda = () => {
+    let options = {
+      method: "POST",
+      headers: {"content-type":"application/JSON"},
+      body: JSON.stringify({
+        "slug": "sean-hammond",
+        "id": 0
+      })
+    }
+    fetch(store.baseURL + "/agendas/sean-hammond" , options)
+    .then((resp)=>resp.json())
+    .then((data)=>console.log("createAgenda data :". data))
+  }
+
+  useEffect(
+    () => {
+    createAgenda()
+  },[] // the sq.brackets make useEffect is triggered every time page loads
+);
+
   return (
     <div className="text-center mt-5">
       <h1>Hello Rigo!!</h1>
@@ -15,23 +35,32 @@ export const Home = () => {
       </p>
       <Link to="/test">Go To Test Page</Link>
       <div className="m-3">
-        <button className="btn btn-primary" onClick={()=>{
-        dispatch({
-          type: "set-fname", 
-          payload: "Alex"
-        })
-      }} >{store.fname}</button>
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            dispatch({
+              type: "set-fname",
+              payload: "Alex",
+            });
+          }}
+        >
+          {store.fname}
+        </button>
       </div>
-      
+
       <div className="m-3">
-        <button className="btn btn-primary" onClick={()=>{
-        dispatch({
-          type: "set-lname",
-          payload: "Ayala"
-        })
-      }} >{store.lname}</button>
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            dispatch({
+              type: "set-lname",
+              payload: "Ayala",
+            });
+          }}
+        >
+          {store.lname}
+        </button>
       </div>
-      
     </div>
   );
 };
